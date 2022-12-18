a57d5a31086bfb3d5316e2f09cee1cce65af9dbc
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-18 12:28:24 +0300

    feat: store + test

--- store/todo.ts
+++ store/todo.ts
@@ -1,12 +1,66 @@
 import { defineStore } from "pinia";
+import { v4 as uuid } from "uuid"
 
-const state = () =>({
-  items: []
-})
+export interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export type Todos = Todo[] | undefined[];
 
-const getters = {};
+export interface TodoAdd {
+  title: string
+}
+export interface TodoUpdate {
+  title?: string;
+  done?: boolean;
+}
+export interface TodoState {
+  items: Todos;
+}
 
-const actions = {};
+const state = (): TodoState => ({
+  items: [],
+});
+
+const getters = {
+  getById: (state: TodoState) => {
+    return (id: string) =>
+      state.items.find((item) => !!item && (item as Todo).id === id);
+  },
+  getOrderedTodos: (state: TodoState) => {
+    return [...state.items].sort(
+      (a, b) =>
+        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    );
+  },
+};
+
+const actions = {
+  add(partialTodo: TodoAdd) {
+    const todo: Todo = {
+      id: uuid(),
+      ...partialTodo,
+      done: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    this.items.push(todo)
+  },
+  remove(id: string) {
+    this.items = this.items.filter(todo => todo.id !== id)
+  },
+  update(id: string, update: TodoUpdate) {
+    const index = this.items.findIndex((item) => item.id === id);
+    this.items[index] = {
+      ...this.items[index],
+      ...update,
+      updatedAt: new Date()
+    }
+  }
+};
 
 export const useTodoStore = defineStore('todoStore', {
   state,
