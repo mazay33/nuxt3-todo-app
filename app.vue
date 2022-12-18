235b3fcb8881ac540a9d7653e33bf93c4329273f
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-18 12:30:36 +0300

    feat: update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class=" w-full min-h-screen bg-gray-100">
+    <section class="text-center pt-5">
+      <h1 class="text-4xl text-gray-700">What are we doing today?</h1>
+    </section>
+    <section class="rounded w-9/12 p-5 max-w-lg mx-auto focus:outline-blue-300 flex items-center justify-center mt-7 bg-white shadow">
+      <form @submit.prevent>
+        <input class=" rounded py-2 px-4 border-blue-200" placeholder="Add a todo" type="text">
+        <button class="rounded border border-blue-200 py-2 px-4 ml-3 hover:bg-blue-200 transition-all duration-200">Add Todo</button>
+      </form>
+    </section>
   </div>
 </template>
